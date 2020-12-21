0ad5b519f937bf7b6e80fc320876989d5be4bd16
Alexis Vanacker <alexis.vanacker@concordnow.com>
2020-12-21 14:38:50 +0100

    Catch Exception and log it when loading json

--- src/li.py
+++ src/li.py
@@ -12,27 +12,31 @@ def stream(users):
     """
     userpayload = ",".join(users)
     print("Starting streaming games for {}".format(users))
-    r = requests.post(GAMES_ENDPOINT, data=userpayload, stream=True)
-
-    for line in r.iter_lines(chunk_size=1):
-        if line:
-            print(line)
-            yield line
+    with requests.post(GAMES_ENDPOINT, data=userpayload, stream=True) as r:
+        for line in r.iter_lines(chunk_size=1):
+            if line:
+                print(line)
+                yield line
 
 
 def game_to_message(line):
-    game = json.loads(line)
-    player_white = game['players']['white']['userId']
-    player_black=game['players']['black']['userId']
-    # TODO Have a real Game object here
-    game_ended = game["status"] != 20
-    game_state = "ended" if game_ended else "started"
-    url = "https://lichess.org/" + game["id"]
-    # Hack to break cache in discord with the Embed
-    if game_ended:
-        url += "?1"
-    return "Game {} between {} and {}, URL is: {}".format(game_state,
-                                                          player_white,
-                                                          player_black,
-                                                          url)
+    try:
+        game = json.loads(line)
+        player_white = game['players']['white']['userId']
+        player_black=game['players']['black']['userId']
+        # TODO Have a real Game object here
+        game_ended = game["status"] != 20
+        game_state = "ended" if game_ended else "started"
+        url = "https://lichess.org/" + game["id"]
+        # Hack to break cache in discord with the Embed
+        if game_ended:
+            url += "?1"
+            return "Game {} between {} and {}, URL is: {}".format(game_state,
+                                                                  player_white,
+                                                                  player_black,
+                                                                  url)
+    except Exception as e:
+        print("Couldn't decode " + str(line)+", Exception: " + str(e))
+
+
 
